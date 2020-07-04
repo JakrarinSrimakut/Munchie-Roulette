23e3d08d2dae9568627c104224c58c3d629ab397
Jack <gungravefire@yahoo.com>
2020-07-04 14:16:17 -0500

    Animation for roulette implemented

--- app/src/main/java/com/example/h/munchieroulette/MainActivity.kt
+++ app/src/main/java/com/example/h/munchieroulette/MainActivity.kt
@@ -16,6 +16,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
+    private var spinning: Boolean = false
+    private var lastDir: Float = 0f
+    private var random: Random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +30,40 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
         when(v.id){
             cLayoutRoulette.id -> {
-                cLayoutRoulette.animate().rotation(10f).start()
-                Log.d("Test", "clicked")}
+                spinRoulette()
+            }
             else -> {
             }
+        }
+    }
+
+    fun spinRoulette(){
+        if(!spinning){
+            var newDir: Float = random.nextInt(1800).toFloat()
+            var pivotX: Float = cLayoutRoulette.getWidth()/2f;
+            var pivotY: Float = cLayoutRoulette.getHeight()/2f;
+
+            var rotate: Animation = RotateAnimation(lastDir, newDir, pivotX, pivotY)
+            rotate.setDuration(2500)
+            rotate.setFillAfter(true)
+
+            rotate.setAnimationListener(object: AnimationListener{
+                override fun onAnimationStart(p0: Animation?) {
+                    spinning = true
+                }
+
+                override fun onAnimationEnd(p0: Animation?) {
+                    spinning = false
+                }
+
+                override fun onAnimationRepeat(p0: Animation?) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+            lastDir = newDir //next spin know where to start off
+            cLayoutRoulette.startAnimation(rotate)
+            Log.d("Test", "clicked")
         }
     }
 }
